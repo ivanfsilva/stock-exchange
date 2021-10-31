208a01d84c7ac38eaeeb1b337c9da7c9d6744c73
ivanfsilva <ivanfs27@gmail.com>
2021-10-31 16:15:40 -0300

    creating first view

---
+++ frontend/app/views/negocicoes-view.ts
@@ -0,0 +1,19 @@
+export class NegocicoesView {
+    template(): string {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                      <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                      </tr>      
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        `;
+    }
+}
