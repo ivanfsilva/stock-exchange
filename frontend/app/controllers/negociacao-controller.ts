c13cff8d8feda7c587b9b42705627d301b2887bf
ivanfsilva <ivanfs27@gmail.com>
2021-11-01 14:37:52 -0300

    implementing removeComments in the tsconfig file

--- frontend/app/controllers/negociacao-controller.ts
+++ frontend/app/controllers/negociacao-controller.ts
@@ -8,6 +8,7 @@ export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegocicoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
@@ -16,6 +17,7 @@ export class NegociacaoController {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+
         this.negociacoesView.update(this.negociacoes);
     }
 
